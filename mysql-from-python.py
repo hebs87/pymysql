914e7603eb3c9b84b6b70d4f0fafeefad9638331
Ubuntu <ubuntu@ip-172-31-93-217.ec2.internal>
2019-06-04 14:55:37 +0000

    Used dict cursor to return row data in dictionaries

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -13,11 +13,19 @@ connection = pymysql.connect(host='localhost',
                             db='Chinook')
 
 try:
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
+    # cursor() is default and returns row data as tuples
+    # cursor(pymysql.cursors.DictCursor) returns data in dictionary and includes the column names
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        sql = "SELECT * FROM Genre;"
         cursor.execute(sql)
+        '''
+        This is for testing
         result = cursor.fetchall()
         print(result)
+        '''
+        # To iterate over each row and return the data, we need a for loop
+        for row in cursor:
+            print(row)
 
 finally:
     # Close the connection to MySQL, regardless of whether code works
